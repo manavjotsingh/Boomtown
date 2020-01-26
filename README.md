be471de0c1324427edef5faa07ca89dfeb64f36e
Manav Singh <manavjot13@gmail.com>
2020-01-26 15:34:54 -0800

    remove extra code and comments from pg-resoure and start readme

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # Boomtown 🏙
 
+## Technologies Used
+
+GraphQl 
+
+- open-source data query and manipulation language for APIs, and a runtime for fulfilling queries with existing data.
+
+Express 
+
+- a web application framework for Node.js
+
+Apollo Server 
+
+- way to quickly build a production-ready, self-documenting API for GraphQL clients, using data from any source.
+
+Postgres 
+
+- is a free and open-source relational database management system emphasizing extensibility and technical standards compliance.
+
+Node.js
+
+- is an open-source, cross-platform, JavaScript runtime environment that executes JavaScript code outside of a browser.
+
 ## Server
 
 Commands must be run from the `server` directory:
--- server/api/pg-resource.js
+++ server/api/pg-resource.js
@@ -10,7 +10,7 @@ module.exports = postgres => {
     async createUser({ fullname, email, password }) {
       const newUserInsert = {
         text: `INSERT INTO users(fullname, email, password) 
-        VALUES($1, $2, $3) RETURNING *`, // @TODO: Authentication - Server
+        VALUES($1, $2, $3) RETURNING *`,
         values: [fullname, email, password],
       };
       try {
@@ -29,7 +29,7 @@ module.exports = postgres => {
     },
     async getUserAndPasswordForVerification(email) {
       const findUserQuery = {
-        text: `SELECT * FROM users WHERE email=$1`, // @TODO: Authentication - Server
+        text: `SELECT * FROM users WHERE email=$1`,
         values: [email],
       };
       try {
@@ -42,7 +42,7 @@ module.exports = postgres => {
     },
     async getUserById(id) {
       const findUserQuery = {
-        text: `SELECT id, email, fullname, bio FROM users WHERE id=$1`, // @TODO: Basic queries
+        text: `SELECT id, email, fullname, bio FROM users WHERE id=$1`,
         values: [id],
       };
       try {
@@ -52,7 +52,6 @@ module.exports = postgres => {
       } catch (e) {
         throw "User or Password was incorrect.";
       }
-      // -------------------------------
     },
     async getItems(idToOmit) {
       const items = await postgres.query({
@@ -95,94 +94,42 @@ module.exports = postgres => {
       return tags.rows;
     },
 
-
-
-
-
-    /**
-       *  @TODO: Adding a New Item
-       *
-       *  Adding a new Item requires 2 separate INSERT statements.
-       *  
-       *  All of the INSERT statements must:
-       *  1) Proceed in a specific order.
-       *  2) Succeed for the new Item to be considered added
-       *  3) If any of the INSERT queries fail, any successful INSERT
-       *     queries should be 'rolled back' to avoid 'orphan' data in the database.
-       *
-       *  To achieve #3 we'll ue something called a Postgres Transaction!
-       *  The code for the transaction has been provided for you, along with
-       *  helpful comments to help you get started.
-       *
-       *  Read the method and the comments carefully before you begin.
-       */
-
-    /**
-             * Begin transaction by opening a long-lived connection
-             * to a client from the client pool.
-             * - Read about transactions here: https://node-postgres.com/features/transactions
-             */
-
-
-
     async saveNewItem({ item, user }) {
 
       return new Promise((resolve, reject) => {
         /** */
         postgres.connect((err, client, done) => {
           try {
-            // Begin postgres transaction
             client.query("BEGIN", async err => {
               const { title, description, tags } = item;
-              // Generate new Item query
-              // @TODO
-              // -------------------------------
+
               const createItemQuery = {
                 text: `INSERT INTO items (title, description, ownerid) VALUES ($1, $2, $3) returning *;`
                 ,
                 values: [title, description, user.id],
               };
-
-              // Insert new Item
               const newItem = await postgres.query(createItemQuery);
               const itemid = newItem.rows[0].id;
-              // return createItems.rows;
-              // @TODO
-              // -------------------------------
-
-              // Generate tag relationships query (use the'tagsQueryString' helper function provided)
-              // @TODO
-              // -------------------------------
               let itemTagsValue = tagsQueryString([...tags], itemid, results)
               const createItemtagsQuery = {
                 text: `INSERT INTO itemtags (itemid, tagid) VALUES ${values}`
                 ,
                 values: tags.map(tag => tag.id),
               };
-
-              // Insert tags
-              // @TODO
-              // -------------------------------
-
-              // Commit the entire transaction!
               client.query("COMMIT", err => {
                 if (err) {
                   throw err;
                 }
-                // release the client back to the pool
                 done();
-                // Uncomment this resolve statement when you're ready!
                 resolve(newItem.rows[0])
                 // -------------------------------
               });
             });
           } catch (e) {
-            // Something went wrong
             client.query("ROLLBACK", err => {
               if (err) {
                 throw err;
               }
-              // release the client back to the pool
               done();
             });
             switch (true) {
